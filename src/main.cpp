174b48a0010cc529b86a1753483f6af9d041ee2d
wayne <wayne.clancy@news.co.uk>
2014-01-27 20:55:26 +0000

    Added Images -WC

--- src/main.cpp
+++ src/main.cpp
@@ -3056,7 +3056,7 @@ bool static AlreadyHave(const CInv& inv)
 // The message start string is designed to be unlikely to occur in normal data.
 // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
 // a large 4-byte int at any alignment.
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb }; // Creepercoin: increase each by adding 2 to bitcoin's value.
+unsigned char pchMessageStart[4] = { 0x1b, 0xc2, 0xb2, 0xda }; // Creepercoin: increase each by adding 2 to bitcoin's value.
 
 
 void static ProcessGetData(CNode* pfrom)
